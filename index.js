269bfe0da2a3788108a227713551c9c614a05df6
Marcel371 <marcel.aurelius.angkasa@gmail.com>
2026-01-22 21:20:11 +0700

    Add basic arithmetic functions for addition, subtraction, multiplication, and division

---
+++ index.js
@@ -0,0 +1,18 @@
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+
+function kali(a, b) {
+  return a * b;
+}
+
+function bagi(a, b) {
+  if (b === 0) {
+    return "Error: pembagian dengan nol!";
+  }
+  return a / b;
+}
